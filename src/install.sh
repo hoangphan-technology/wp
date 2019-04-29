df1b33a51dad4da29c9c522079317729fef899f7
hoang.phan <hoang.phan@fidesio.com>
2019-04-29 08:40:25 +0700

    init script

---
+++ src/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+composer install
+
+source ./.env
+
+#php wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root --extra-php <<PHP
+#define( 'WP_DEBUG', true );
+#define( 'WP_DEBUG_LOG', true );
+#PHP
+
+php wp-cli.phar core install --url=$WP_HOME --title=Example --admin_user=$WP_ADMIN --admin_password=$WP_PWD --admin_email=$WP_EMAIL --allow-root
